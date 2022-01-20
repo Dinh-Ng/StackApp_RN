ca405c2319853972e306fbbb8d92eb112ac68501
Dinh Nguyen <dinhquangnguyen1997@gmail.com>
2022-01-20 16:27:21 +0700

    Add babel-plugin-module-resolver and config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,26 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        cwd: ['packagejson'],
+        root: ['./src'],
+        extensions: ['.ts', '.tsx', '.js', '.json'],
+        alias: {
+          '@icons': './assets/icons',
+          '@fonts': './assets/fonts',
+          '@images': './assets/images',
+
+          '@navigation': './navigation',
+          '@core': './shared/core',
+          '@styles': './shared/styles',
+          '@services': './services',
+
+          '@store': './store',
+          '@modules': './modules',
+        },
+      },
+    ],
+  ],
 };
