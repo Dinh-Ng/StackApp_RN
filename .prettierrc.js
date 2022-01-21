a69439e57e04f50bfcf4fcfa0a290fd0ffe4e41e
Dinh Nguyen <dinhquangnguyen1997@gmail.com>
2022-01-21 16:48:58 +0700

    change prettier settings

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,9 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
   singleQuote: true,
+  semi: false,
   trailingComma: 'all',
-  arrowParens: 'avoid',
+  printWidth: 120,
+  tabWidth: 2,
 };
